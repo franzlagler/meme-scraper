bb0ae7af665a59ce0fbf7c4b8cb58157f2ea8e71
Franz Lagler <franz.lagler@outlook.com>
2021-09-08 17:35:04 +0200

    Add function for custom memes

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 import fs from 'node:fs';
 import fetch from 'node-fetch';
 
+const folderName = './memes';
+
 // 1. Download  memes and put them in the required directory
 
 async function downloadMemes(urlList) {
-  const folderName = './memes';
   // Check if directory exists
   if (!fs.existsSync(folderName)) {
     fs.mkdir(folderName, (err) => {
@@ -31,7 +32,7 @@ async function downloadMemes(urlList) {
 
 // 2. Fetches all the memes and puts them into an array
 
-async function CreateMemeList() {
+async function createMemeList() {
   const response = await fetch(
     'https://memegen-link-examples-upleveled.netlify.app/',
   );
@@ -46,6 +47,46 @@ async function CreateMemeList() {
   downloadMemes(filteredFoundUrls);
 }
 
-// 3. Run Function that completes task
+// 3. Create Custom Meme
 
-CreateMemeList();
+async function createCustomMeme() {
+  const template = process.argv[2];
+  const text1 = process.argv[3];
+  const text2 = process.argv[4];
+
+  const customMeme = await fetch(
+    `https://api.memegen.link/images/${template}/${text1}/%20${text2}.jpg`,
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    },
+  );
+
+  if (!fs.existsSync(folderName)) {
+    fs.mkdir(folderName, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+
+  const file = fs.createWriteStream(`${folderName}/custom-meme.jpg`);
+
+  customMeme.body.pipe(file, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+// 4. Check which function to run
+
+// If no additional input
+if (!process.argv[2]) {
+  createMemeList();
+}
+// If additional input
+else {
+  createCustomMeme();
+}
