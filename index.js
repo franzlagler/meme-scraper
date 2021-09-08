4ab4acb102fbcf308d7e70cf56e1550db7185e5a
Franz Lagler <franz.lagler@outlook.com>
2021-09-08 18:00:39 +0200

    Add progress bar

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+// Progress Bar is not finished yet
+
 // 0. Import the require packages
 
 import fs from 'node:fs';
+import cliProgress from 'cli-progress';
 import fetch from 'node-fetch';
 
-// 1. Download  memes and put them in the required directory
+// 1. Set Progress Bar
+
+const progressBar = new cliProgress.SingleBar(
+  {},
+  cliProgress.Presets.shades_classic,
+);
+
+progressBar.start(200, 0);
+
+// 2. Download  memes and put them in the required directory
 
 async function downloadMemes(urlList) {
   const folderName = './memes';
@@ -15,26 +27,30 @@ async function downloadMemes(urlList) {
       }
     });
   }
-  // Download 10 memes and put them in the direcotry
+  // Download memes and put them in the direcotry
   for (let i = 0; i <= 9; i++) {
     const file = fs.createWriteStream(`${folderName}/memes${i + 1}.jpg`);
     const fetchedImg = await fetch(urlList[i], (err) => {
       if (err) {
         console.log(err);
       }
+      progressBar.increment(15);
     });
     fetchedImg.body.pipe(file, (err) => {
       console.log(err);
     });
   }
+  progressBar.increment(30);
+  progressBar.stop();
 }
 
-// 2. Fetch all memes and put them in an array
+// 3. Fetch all memes and put them in an array
 
 async function createMemeList() {
   const response = await fetch(
     'https://memegen-link-examples-upleveled.netlify.app/',
   );
+  progressBar.increment(20);
   const body = await response.text();
   // Extract Image Urls from HTML string
   const foundUrls = body.match(/https:\/\/api\.memegen\.link\/images.*\.jpg/g);
@@ -46,44 +62,23 @@ async function createMemeList() {
   downloadMemes(filteredFoundUrls);
 }
 
-// 3. Create Custom Meme
+// 4. Create Custom Meme
 
 async function createCustomMeme() {
   const template = process.argv[2];
   const text1 = process.argv[3];
   const text2 = process.argv[4];
 
-  const customMeme = await fetch(
+  const customMemeUrl = [
     `https://api.memegen.link/images/${template}/${text1}/%20${text2}.jpg`,
-    (err) => {
-      if (err) {
-        console.log(err);
-      }
-    },
-  );
+  ];
 
-  const folderName = './memes';
+  progressBar.increment(20);
 
-  // Check if directory exists
-
-  if (!fs.existsSync(folderName)) {
-    fs.mkdir(folderName, (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-
-  const file = fs.createWriteStream(`${folderName}/custom-meme.jpg`);
-
-  customMeme.body.pipe(file, (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
+  downloadMemes(customMemeUrl);
 }
 
-// 4. Check Which Function to Run
+// 5. Check Which Function to Run
 
 // If no additional input
 if (!process.argv[2]) {
