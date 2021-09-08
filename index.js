9b297171fd8cb996bfce6096cd269d70d121f91d
Franz Lagler <franz.lagler@outlook.com>
2021-09-08 17:13:21 +0200

    Add comments so that code is more understandable

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
+// 0. Import the require packages
+
 import fs from 'node:fs';
 import fetch from 'node-fetch';
 
-async function fetchAndUploadImages() {
-  const response = await fetch(
-    'https://memegen-link-examples-upleveled.netlify.app/',
-  );
-  const body = await response.text();
-  const foundUrls = body.match(/https:\/\/api\.memegen\.link\/images.*\.jpg/g);
-  const filteredFoundUrls = foundUrls.filter((el, index) => {
-    return foundUrls.indexOf(el) === index;
-  });
+// 1. Download  memes and put them in the required directory
 
+async function downloadMemes(urlList) {
   const folderName = './memes';
-
+  // Check if directory exists
   if (!fs.existsSync(folderName)) {
     fs.mkdir(folderName, (err) => {
       if (err) {
@@ -20,10 +15,10 @@ async function fetchAndUploadImages() {
       }
     });
   }
-
+  // Download 10 memes and put them into the direcotry
   for (let i = 0; i <= 9; i++) {
     const file = fs.createWriteStream(`${folderName}/memes${i + 1}.jpg`);
-    const fetchedImg = await fetch(filteredFoundUrls[i], (err) => {
+    const fetchedImg = await fetch(urlList[i], (err) => {
       if (err) {
         console.log(err);
       }
@@ -34,4 +29,23 @@ async function fetchAndUploadImages() {
   }
 }
 
-fetchAndUploadImages();
+// 2. Fetches all the memes and puts them into an array
+
+async function fetchMemes() {
+  const response = await fetch(
+    'https://memegen-link-examples-upleveled.netlify.app/',
+  );
+  const body = await response.text();
+  // Extract Image Urls from HTML string
+  const foundUrls = body.match(/https:\/\/api\.memegen\.link\/images.*\.jpg/g);
+  // Filter out Urls that exist twice
+  const filteredFoundUrls = foundUrls.filter((el, index) => {
+    return foundUrls.indexOf(el) === index;
+  });
+
+  downloadMemes(filteredFoundUrls);
+}
+
+// 3. Run Function that completes task
+
+fetchMemes();
